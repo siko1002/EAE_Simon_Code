9d4216487999f6822ecf72951955a33c300d5c62
siko1002 <siko1002@stud.hs-kl.de>
2022-12-22 09:20:02 +0100

    Add: Extend API

--- app/src/main/java/com/example/booklibrary/AuthorRequestApi.java
+++ app/src/main/java/com/example/booklibrary/AuthorRequestApi.java
@@ -37,7 +37,41 @@ public class AuthorRequestApi {
         if (authoren.size() > 0) {
             System.out.println(authoren.get(0));
             return authoren.get(0);
-        }else
+        } else
+            return null;
+    }
+
+    public static List<Author> getAuthorListByName(String name) throws IOException {
+        name.toLowerCase();
+        name = name.replace(" ", "%20");
+
+        String urlString = "https://openlibrary.org/search/authors.json?q=" + name;
+
+        URL url = new URL(urlString);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+
+        String jsonString = content.toString();
+        // System.out.println(jsonString);
+        List<Author> authoren = JsonStringToAuthorObject.parseJsonToAuthor(jsonString);
+
+        for (Author a : authoren) {
+            System.out.println(a.toString());
+        }
+
+        if (authoren.size() > 0) {
+            System.out.println("Anzahl der Ergebnisse: " + authoren.size());
+            return authoren;
+        } else
             return null;
     }
 }
